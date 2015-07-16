23deba71b33486ac3bb1c52a4d01291f677a0d17
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-16 19:02:47 +0800

    Enums

---
+++ src/test/com/digiwes/product/spec/ProductSpecCharacteristicValueTest.java
@@ -0,0 +1,33 @@
+package com.digiwes.product.spec;
+
+import com.digiwes.basetype.TimePeriod;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by zhaoyp on 2015/7/16.
+ */
+public class ProductSpecCharacteristicValueTest {
+    private ProductSpecCharacteristicValue memoryCharValue;
+    private ProductSpecCharacteristicValue charValue;
+    private static TimePeriod validFor;
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAssociate() throws Exception {
+
+    }
+
+    @Test
+    public void testRetrieveRelatedCharValue() throws Exception {
+
+    }
+
+    @Test
+    public void testRetrieveRelatedCharValue1() throws Exception {
+
+    }
+}
