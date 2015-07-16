9a11056d3aab54b1932c84a74822ff6b054a8369
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-16 16:41:47 +0800

    Enums

--- src/main/java/com/digiwes/common/enums/ProdSpecErrorCode.java
+++ src/main/java/com/digiwes/common/enums/ProdSpecErrorCode.java
@@ -26,7 +26,9 @@ public enum ProdSpecErrorCode{
     PROD_SPEC_CHAR_VALUE_HAS_RELATED_TO_CURRENT(1019,"CharacteristicValue has been established associate relationship in the specified time"),
     PROD_SPEC_CHAR_USE_NAME_IS_NULL(1020,"ProductSpecCharUse'name is null"),
     PROD_SPEC_CHAR_USE_TIME_NOT_BELONG_OF_CHAR_TIME(1021,"the specify time not belong of ProductSPecifcationChar's validFor"),
-    PROD_SPEC_CHAR_HAS_ATTACHED_TO_SPEC(1022,"the char has benn attached to the ProdSpec");
+    PROD_SPEC_CHAR_HAS_ATTACHED_TO_SPEC(1022,"the char has benn attached to the ProdSpec"),
+    PROD_SPEC_CHAR_VALUE_USE_TIME_NOT_BELONG_OF_CHARVALUE_TIME(1023,"the specify time not belong of ProductSPecifcationCharValue's validFor"),
+    PROD_SPEC_NOT_USED_CURRENT_CHAR(1024,"the char not be used by this Spec")  ;
 
 
     private  int code;
