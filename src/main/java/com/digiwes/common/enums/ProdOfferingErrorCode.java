65085b1ad7c1ac8be082b95edc213a0199f1c09f
liurl3 <liurl3@asiainfo.com>
2015-07-16 14:26:51 +0800

    添加错误码

--- src/main/java/com/digiwes/common/enums/ProdOfferingErrorCode.java
+++ src/main/java/com/digiwes/common/enums/ProdOfferingErrorCode.java
@@ -7,7 +7,9 @@ public enum ProdOfferingErrorCode {
     PROD_OFFERING_NAME_IS_NULL_OR_EMPTY(2001,"name is null or empty"),
     PROD_OFFERING_OFFERING_IS_NULL(2002,"offering is null or empty"),
     PROD_OFFERING_RELATIONSHIP_TYPE_IS_NULL_OR_EMPTY(2003,"relationshpType is null or empty"),
-    BUNDLED_PROD_OFFERING_LOWERLIMIT_GREATER_UPPERLIMIT(2004,"lowerlimit is greater than upperlimit");
+    PROD_OFFERING_RELATIONSHIP_ALREADY_EXISTING(2004,"relationship is already existing"),
+    PROD_OFFERING_ASSOCIATE_ITSELF(2005,"Its associated himself"),
+    BUNDLED_PROD_OFFERING_LOWERLIMIT_GREATER_UPPERLIMIT(2006,"lowerlimit is greater than upperlimit");
     private  int code;
     private String message;
     private ProdOfferingErrorCode(int code,String message){
