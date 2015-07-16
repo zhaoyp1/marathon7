ca912e6de5d29eddbbfeeb109546c443e807c046
dongwh <dongwh@asiainfo.com>
2015-07-16 19:24:20 +0800

    ProdSpec

---
+++ src/test/java/com/digiwes/product/offering/ProductOfferingTest.java
@@ -0,0 +1,24 @@
+package com.digiwes.product.offering;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dongwh on 2015-7-16.
+ */
+public class ProductOfferingTest {
+
+    @org.junit.Test
+    public void testAssociate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRetrieveRelatedOffering() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRetrieveRelatedOffering1() throws Exception {
+
+    }
+}
