8c02f55192cad988b4801ea80c21c3f3e59243e5
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-16 10:33:37 +0800

    Enums

--- src/main/java/com/digiwes/common/enums/ProdSpecErrorCode.java
+++ src/main/java/com/digiwes/common/enums/ProdSpecErrorCode.java
@@ -6,8 +6,7 @@ import com.digiwes.product.spec.ProdSpecCharValueUse;
  * Created by zhaoyp on 2015/7/14.
  */
 public enum ProdSpecErrorCode{
-    PROD_SPEC_NAME_IS_NULL(1001,"name is null"),
-    PROD_SPEC_NAME_IS_EMPTY(1002,"name is empty"),
+    PROD_SPEC_NAME_IS_NULL_OR_EMPTY(1001,"name is null or empty"),
     PROD_SPEC_IS_NULL(1004,"productSpecification is null"),
     PROD_SPEC_RELATIONSHIP_TYPE_IS_NULL(1003,"relationshipType is null"),
     PROD_SPEC_EQUALS_TO_CURRENT(1005,"the designated productSpecification equal to current value"),
