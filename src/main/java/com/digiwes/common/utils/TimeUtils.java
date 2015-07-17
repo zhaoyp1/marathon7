adf0e6986ff3026c31efae8b63ec1ec666324b55
nisx <nisx@asiainfo.com>
2015-07-17 10:37:29 +0800

    create TimeUtils

---
+++ src/main/java/com/digiwes/common/utils/TimeUtils.java
@@ -0,0 +1,49 @@
+package com.digiwes.common.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * Created by Nisx on 2015/7/17.
+ */
+public class TimeUtils {
+
+    public static final String Y4MMDD = "yyyyMMdd";
+    public static final String Y2MMDD = "yyMMdd";
+    public static final String Y4_MM_DD = "yyyy-MM-dd";
+    public static final String Y2_MM_DD = "yy-MM-dd";
+    public static final String Y4MMDD_TIME = "yyyyMMdd HH:mm:ss";
+    public static final String Y2MMDD_TIME = "yyMMdd HH:mm:ss";
+    public static final String Y4_MM_DD_TIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String Y2_MM_DD_TIME = "yy-MM-dd HH:mm:ss";
+
+    private static final String[] fullPattern = new String[] {
+            Y4MMDD,
+            Y2MMDD,
+            Y4_MM_DD,
+            Y2_MM_DD,
+            Y4MMDD_TIME,
+            Y2MMDD_TIME,
+            Y4_MM_DD_TIME,
+            Y2_MM_DD_TIME,
+    };
+    public static Date parseDate(String strDate) throws Exception {
+        return parseDate(strDate,null);
+    }
+    public static Date parseDate(String strDate, String pattern) throws Exception {
+        if (StringUtils.isEmpty(strDate)) {
+            throw new IllegalArgumentException("parameter [strDate] must not be null or empty");
+        }
+        String[] patterns;
+        if (pattern == null) {
+            patterns = new String[] {
+                    pattern
+            };
+        } else {
+            patterns = fullPattern;
+        }
+        return org.apache.commons.lang.time.DateUtils.parseDate(strDate, patterns);
+    }
+}
---
+++ src/test/java/com/digiwes/basetype/TimePeriodTest.java
@@ -0,0 +1,96 @@
+package com.digiwes.basetype;
+
+import com.digiwes.common.utils.TimeUtils;
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nisx on 2015/7/17.
+ */
+public class TimePeriodTest {
+
+    @Test
+    public void testCreateTimePeriod() throws Exception {
+        TimePeriod timePeriod = null;
+        //Case£ºParameter null test
+        try {
+            timePeriod = new TimePeriod(null, null);
+            fail("parameter null test failed");
+        } catch (Exception e) {
+
+        }
+        try {
+            timePeriod = new TimePeriod(new Date(), null);
+            fail("parameter null test failed");
+        } catch (Exception e) {
+
+        }
+        try {
+            timePeriod = new TimePeriod(null, new Date());
+            fail("parameter null test failed");
+        } catch (Exception e) {
+
+        }
+
+        //Case£ºstartDateTime > endDateTime
+        try {
+            Date startDate = TimeUtils.parseDate("20150717", "yyyyMMdd");
+            Date endDate = TimeUtils.parseDate("20150716", "yyyyMMdd");
+            timePeriod = new TimePeriod(startDate, endDate);
+            fail("startDateTime > endDateTime is not allowed");
+        } catch (Exception e) {
+
+        }
+        //Case£ºstartDateTime = endDateTime
+        try {
+            Date startDate = TimeUtils.parseDate("20150717", "yyyyMMdd");
+            Date endDate = TimeUtils.parseDate("20150717", "yyyyMMdd");
+            timePeriod = new TimePeriod(startDate, endDate);
+        } catch (Exception e) {
+            fail("startDateTime = endDateTime is allowed£¬but test error!");
+        }
+        //Case£ºstartDateTime < endDateTime
+        try {
+            Date startDate = TimeUtils.parseDate("20150717", "yyyyMMdd");
+            Date endDate = TimeUtils.parseDate("20150719", "yyyyMMdd");
+            timePeriod = new TimePeriod(startDate, endDate);
+            assertEquals("startDateTime is incorrect",startDate, timePeriod.getStartDateTime());
+            assertEquals("endDateTime is incorrect",endDate, timePeriod.getEndDateTime());
+        } catch (Exception e) {
+            fail("startDateTime < endDateTime is allowed£¬but test error!");
+        }
+    }
+    @Test
+    public void testIsInTimePeriod() throws Exception {
+
+    }
+
+    @Test
+    public void testIsOverlap() throws Exception {
+
+    }
+
+    @Test
+    public void testIsInTimePeriod1() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+}
