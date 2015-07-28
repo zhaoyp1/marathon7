211004312430e5b46d1ae337d2be200b12a924d7
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-28 10:23:40 +0800

    Enums

---
+++ src/main/java/com/digiwes/product/resource/api/CatalogManagementResource.java
@@ -0,0 +1,14 @@
+package com.digiwes.product.resource.api;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by zhaoyp on 2015/7/28.
+ */
+@Path("/catalogManagement")
+@Produces({ "application/json"})
+public class CatalogManagementResource {
+
+
+}
