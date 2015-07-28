cea80b4baabe077ed03b2c53496ec1cd6f511bc2
dongwh <dongwh@asiainfo.com>
2015-07-28 16:38:25 +0800

    ProdSpec

--- src/main/java/com/digiwes/product/resource/utils/ConvertUtil.java
+++ src/main/java/com/digiwes/product/resource/utils/ConvertUtil.java
@@ -3,11 +3,14 @@ package com.digiwes.product.resource.utils;
 import com.digiwes.product.offering.BundledProdOfferOption;
 import com.digiwes.product.offering.ProductOffering;
 import com.digiwes.product.offering.SimpleProductOffering;
+import com.digiwes.product.offering.catalog.ProdCatalogProdOffer;
 import com.digiwes.product.resource.response.BundledProductOffering;
 import com.digiwes.product.resource.response.ProdOffering;
+import com.digiwes.product.resource.response.ProductOfferingPrice;
 import com.digiwes.product.resource.response.ProductSpecificationRef;
 import com.digiwes.product.spec.ProductSpecification;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,8 +46,51 @@ public class ConvertUtil {
             }
         }
         return true;
-
     }
 
+    //covert prodCatalogProdOffering to prodOffering
+    public ProdOffering convertToProdOffering(ProdCatalogProdOffer prodCatalogProdOffer){
+        ProdOffering prodOffering = new ProdOffering();
+        prodOffering.setId(prodCatalogProdOffer.getProdOffering().getId());
+        prodOffering.setValidFor(prodCatalogProdOffer.getValidFor());
+        prodOffering.setName(prodCatalogProdOffer.getProdOffering().getName());
+        prodOffering.setDescription(prodCatalogProdOffer.getProdOffering().getDescription());
+        prodOffering.setLastUpdate(null);
+        prodOffering.setLifecycleStatus(prodCatalogProdOffer.getProdOffering().getStatus());
+        prodOffering.setVersion("1.0.0");
+        //price
+        List<ProductOfferingPrice>  prodOfferingPriceResult = new ArrayList<ProductOfferingPrice>();
+        List<com.digiwes.product.offering.price.ProductOfferingPrice> productOfferingPriceList = prodCatalogProdOffer.getProductOfferingPrice();
+        if(null!= productOfferingPriceList && productOfferingPriceList.size()>0){
+            for(com.digiwes.product.offering.price.ProductOfferingPrice productOfferingPrice : productOfferingPriceList){
+                ProductOfferingPrice prodOfferingPrice = convertToProductOfferingPrice(productOfferingPrice);
+                prodOfferingPriceResult.add(prodOfferingPrice);
+            }
+        }
+        prodOffering.setProductOfferingPrice(prodOfferingPriceResult);
+        if(prodCatalogProdOffer.getProdOffering() instanceof SimpleProductOffering){
+            prodOffering.setProductSpecification(convertToProdSpecRef((SimpleProductOffering)prodCatalogProdOffer.getProdOffering()));
+        }else{
+            prodOffering.setProductSpecification(null);
+        }
+       return null;
+    }
+
+    private ProductOfferingPrice convertToProductOfferingPrice(com.digiwes.product.offering.price.ProductOfferingPrice productOfferingPrice){
+        ProductOfferingPrice prodOfferingPrice = new ProductOfferingPrice();
+        prodOfferingPrice.setVersion("1.0.0");
+        prodOfferingPrice.setDescription(productOfferingPrice.getDescription());
+        prodOfferingPrice.setName(productOfferingPrice.getName());
+        prodOfferingPrice.setPriceType("");
+        return prodOfferingPrice;
+    }
+
+    private ProductSpecificationRef convertToProdSpecRef(SimpleProductOffering simpleProdOffering){
+        ProductSpecificationRef prodSpecRef = new ProductSpecificationRef();
+        prodSpecRef.setId(simpleProdOffering.getId());
+        prodSpecRef.setName(simpleProdOffering.getName());
+        prodSpecRef.setHref("");
+        return prodSpecRef;
+    }
 
 }
