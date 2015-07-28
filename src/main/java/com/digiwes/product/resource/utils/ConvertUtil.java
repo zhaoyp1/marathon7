9811df40fc097bd821db77744558f57836938913
Zhao YunPing <zhaoyp@asiainfo.com>
2015-07-28 16:40:29 +0800

    Enums

--- src/main/java/com/digiwes/product/resource/utils/ConvertUtil.java
+++ src/main/java/com/digiwes/product/resource/utils/ConvertUtil.java
@@ -7,8 +7,12 @@ import com.digiwes.product.resource.response.BundledProductOffering;
 import com.digiwes.product.resource.response.ProdOffering;
 import com.digiwes.product.resource.response.ProductSpecificationRef;
 import com.digiwes.product.spec.ProductSpecification;
+import org.apache.commons.lang.StringUtils;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhaoyp on 2015/7/28.
@@ -45,6 +49,56 @@ public class ConvertUtil {
         return true;
 
     }
+    public static  Map<String,Object> convertObjectToMap(Object thisObj,String fields)
+    {
+        Map map = new HashMap();
+        Class c;
+        String [] fieldArray = null;
+        if(StringUtils.isEmpty(fields)){
+            fieldArray = fields.split(",");
+        }
+        try
+        {
+            c = Class.forName(thisObj.getClass().getName());
+            Method[] m = c.getMethods();
+            for (int i = 0; i < m.length; i++)
+            {
+                String method = m[i].getName();
+                if (method.startsWith("get"))
+                {
+                    try{
+                        Object value = m[i].invoke(thisObj);
+                        if (value != null)
+                        {
+                            if(null != fieldArray){
+                                for(int j=0 ; j<fieldArray.length ; j++){
 
+                                    String key=method.substring(3);
+                                    if(key.equals(fieldArray[j])){
+                                        key=key.substring(0,1)+key.substring(1);
+                                        map.put(method, value);
+                                        break;
+                                    }
+
+                                }
+                            }else{
+                                String key=method.substring(3);
+                                key=key.substring(0,1).toUpperCase()+key.substring(1);
+                                map.put(method, value);
+                            }
+
+                        }
+                    }catch (Exception e) {
+                        System.out.println("error:"+method);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return map;
+    }
 
 }
